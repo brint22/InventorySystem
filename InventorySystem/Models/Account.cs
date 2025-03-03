d39698257032372152aa4930ea3d99229e0f14af
2022-0515 <2022-0515@LAB1-PC09>
2025-03-03 14:58:05 +0800

    f

--- InventorySystem/Models/Account.cs
+++ InventorySystem/Models/Account.cs
@@ -11,6 +11,7 @@ namespace InventorySystem.Models
         public int AccountID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public int person { get; set; }
+        public int Teacher { get; set; }
+      
     }
 }
