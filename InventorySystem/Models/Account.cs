0b503c8e2125526c5de7e3a69bb2129fd728be1e
2022-0515 <2022-0515@LAB1-PC09>
2025-03-04 15:22:18 +0800

    d

--- InventorySystem/Models/Account.cs
+++ InventorySystem/Models/Account.cs
@@ -10,6 +10,6 @@ namespace InventorySystem.Models
     {
         public int AccountID { get; set; }
         public string UserName { get; set; }
-        public string Password1 { get; set; }
+        public string Password { get; set; }
     }
 }
