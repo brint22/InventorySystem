df323b7d50eb86dcf66fc07c09e2d297028f93a0
2023-0796 <2023-0796@LAB1-PC08>
2025-03-18 14:07:41 +0800

    Added addEmployee ribbon form

--- InventorySystem/Employee/AddEmployee.Designer.cs
+++ InventorySystem/Employee/AddEmployee.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEmployee));
             this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
-            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.peProfile = new DevExpress.XtraEditors.PictureEdit();
+            this.btnBrowse = new DevExpress.XtraEditors.SimpleButton();
+            this.lueRole = new DevExpress.XtraEditors.LookUpEdit();
+            this.teUserName = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.teMiddleName = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.teNameExtension = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.teLastName = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.deDateOfBirth = new DevExpress.XtraEditors.DateEdit();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.teLocation = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peProfile.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lueRole.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teUserName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teMiddleName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teNameExtension.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teLastName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deDateOfBirth.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deDateOfBirth.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teLocation.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // ribbon
             // 
-            this.ribbon.ApplicationIcon = null;
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem});
             this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 1;
             this.ribbon.Name = "ribbon";
             this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbon.SelectedPage = this.ribbonPage1;
-            this.ribbon.Size = new System.Drawing.Size(442, 141);
+            this.ribbon.Size = new System.Drawing.Size(864, 49);
             // 
             // ribbonPage1
             // 
@@ -56,23 +85,231 @@
             this.ribbonPageGroup1.Name = "ribbonPageGroup1";
             this.ribbonPageGroup1.Text = "ribbonPageGroup1";
             // 
-            // ribbonStatusBar
+            // peProfile
             // 
-            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 425);
-            this.ribbonStatusBar.Name = "ribbonStatusBar";
-            this.ribbonStatusBar.Ribbon = this.ribbon;
-            this.ribbonStatusBar.Size = new System.Drawing.Size(442, 24);
+            this.peProfile.EditValue = ((object)(resources.GetObject("peProfile.EditValue")));
+            this.peProfile.Location = new System.Drawing.Point(677, 93);
+            this.peProfile.MenuManager = this.ribbon;
+            this.peProfile.Name = "peProfile";
+            this.peProfile.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.peProfile.Properties.Appearance.Options.UseBackColor = true;
+            this.peProfile.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.peProfile.Properties.OptionsMask.MaskType = DevExpress.XtraEditors.Controls.PictureEditMaskType.Circle;
+            this.peProfile.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.peProfile.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Squeeze;
+            this.peProfile.Size = new System.Drawing.Size(148, 144);
+            this.peProfile.TabIndex = 12;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Appearance.BackColor = System.Drawing.Color.DimGray;
+            this.btnBrowse.Appearance.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Appearance.Options.UseBackColor = true;
+            this.btnBrowse.Appearance.Options.UseFont = true;
+            this.btnBrowse.Location = new System.Drawing.Point(706, 243);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(88, 29);
+            this.btnBrowse.TabIndex = 13;
+            this.btnBrowse.Text = "BROWSE";
+            // 
+            // lueRole
+            // 
+            this.lueRole.Location = new System.Drawing.Point(48, 268);
+            this.lueRole.MenuManager = this.ribbon;
+            this.lueRole.Name = "lueRole";
+            this.lueRole.Properties.AutoHeight = false;
+            this.lueRole.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lueRole.Properties.Columns.AddRange(new DevExpress.XtraEditors.Controls.LookUpColumnInfo[] {
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("RoleID", "RoleID", 20, DevExpress.Utils.FormatType.None, "", false, DevExpress.Utils.HorzAlignment.Default, DevExpress.Data.ColumnSortOrder.None, DevExpress.Utils.DefaultBoolean.Default),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("RoleName", "RoleName")});
+            this.lueRole.Properties.NullText = "";
+            this.lueRole.Size = new System.Drawing.Size(207, 23);
+            this.lueRole.TabIndex = 19;
+            // 
+            // teUserName
+            // 
+            this.teUserName.Location = new System.Drawing.Point(48, 125);
+            this.teUserName.MenuManager = this.ribbon;
+            this.teUserName.Name = "teUserName";
+            this.teUserName.Properties.AutoHeight = false;
+            this.teUserName.Size = new System.Drawing.Size(207, 23);
+            this.teUserName.TabIndex = 16;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(50, 103);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(62, 16);
+            this.labelControl1.TabIndex = 17;
+            this.labelControl1.Text = "First Name";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(50, 246);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(25, 16);
+            this.labelControl3.TabIndex = 18;
+            this.labelControl3.Text = "Role";
+            // 
+            // teMiddleName
+            // 
+            this.teMiddleName.Location = new System.Drawing.Point(261, 126);
+            this.teMiddleName.MenuManager = this.ribbon;
+            this.teMiddleName.Name = "teMiddleName";
+            this.teMiddleName.Properties.AutoHeight = false;
+            this.teMiddleName.Size = new System.Drawing.Size(133, 23);
+            this.teMiddleName.TabIndex = 21;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(261, 104);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(74, 16);
+            this.labelControl2.TabIndex = 22;
+            this.labelControl2.Text = "Middle Name";
+            // 
+            // teNameExtension
+            // 
+            this.teNameExtension.Location = new System.Drawing.Point(539, 126);
+            this.teNameExtension.MenuManager = this.ribbon;
+            this.teNameExtension.Name = "teNameExtension";
+            this.teNameExtension.Properties.AutoHeight = false;
+            this.teNameExtension.Size = new System.Drawing.Size(91, 23);
+            this.teNameExtension.TabIndex = 23;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Location = new System.Drawing.Point(539, 104);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(91, 16);
+            this.labelControl4.TabIndex = 24;
+            this.labelControl4.Text = "Name Extension";
+            // 
+            // teLastName
+            // 
+            this.teLastName.Location = new System.Drawing.Point(400, 126);
+            this.teLastName.MenuManager = this.ribbon;
+            this.teLastName.Name = "teLastName";
+            this.teLastName.Properties.AutoHeight = false;
+            this.teLastName.Size = new System.Drawing.Size(133, 23);
+            this.teLastName.TabIndex = 25;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl5.Appearance.Options.UseFont = true;
+            this.labelControl5.Location = new System.Drawing.Point(400, 104);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(60, 16);
+            this.labelControl5.TabIndex = 26;
+            this.labelControl5.Text = "Last Name";
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl6.Appearance.Options.UseFont = true;
+            this.labelControl6.Location = new System.Drawing.Point(50, 174);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(71, 16);
+            this.labelControl6.TabIndex = 28;
+            this.labelControl6.Text = "Date of Birth";
+            // 
+            // deDateOfBirth
+            // 
+            this.deDateOfBirth.EditValue = null;
+            this.deDateOfBirth.Location = new System.Drawing.Point(50, 197);
+            this.deDateOfBirth.MenuManager = this.ribbon;
+            this.deDateOfBirth.Name = "deDateOfBirth";
+            this.deDateOfBirth.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.deDateOfBirth.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.deDateOfBirth.Size = new System.Drawing.Size(205, 28);
+            this.deDateOfBirth.TabIndex = 29;
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl7.Appearance.Options.UseFont = true;
+            this.labelControl7.Location = new System.Drawing.Point(261, 174);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(47, 16);
+            this.labelControl7.TabIndex = 30;
+            this.labelControl7.Text = "Location";
+            // 
+            // teLocation
+            // 
+            this.teLocation.Location = new System.Drawing.Point(261, 197);
+            this.teLocation.MenuManager = this.ribbon;
+            this.teLocation.Name = "teLocation";
+            this.teLocation.Properties.AutoHeight = false;
+            this.teLocation.Size = new System.Drawing.Size(369, 28);
+            this.teLocation.TabIndex = 31;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = System.Drawing.Color.LightSlateGray;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Location = new System.Drawing.Point(50, 317);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(205, 31);
+            this.simpleButton1.TabIndex = 32;
+            this.simpleButton1.Text = "SUBMIT";
             // 
             // AddEmployee
             // 
-            this.ClientSize = new System.Drawing.Size(442, 449);
-            this.Controls.Add(this.ribbonStatusBar);
-            this.Controls.Add(this.ribbon);
-            this.Ribbon = this.ribbon;
-            this.StatusBar = this.ribbonStatusBar;
-            this.Text = "AddEmployee";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 449);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.teLocation);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.deDateOfBirth);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.teLastName);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.teNameExtension);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.teMiddleName);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.lueRole);
+            this.Controls.Add(this.teUserName);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.peProfile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.ribbon);
+            this.IconOptions.Image = ((System.Drawing.Image)(resources.GetObject("AddEmployee.IconOptions.Image")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddEmployee";
+            this.Ribbon = this.ribbon;
+            this.RibbonVisibility = DevExpress.XtraBars.Ribbon.RibbonVisibility.Hidden;
+            this.Text = "Inventory System | Add Employee";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peProfile.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lueRole.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teUserName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teMiddleName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teNameExtension.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teLastName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deDateOfBirth.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deDateOfBirth.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teLocation.Properties)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -80,6 +317,22 @@
         private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
         private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
-        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraEditors.PictureEdit peProfile;
+        private DevExpress.XtraEditors.SimpleButton btnBrowse;
+        private DevExpress.XtraEditors.LookUpEdit lueRole;
+        private DevExpress.XtraEditors.TextEdit teUserName;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit teMiddleName;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit teNameExtension;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.TextEdit teLastName;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.DateEdit deDateOfBirth;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.TextEdit teLocation;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
     }
 }
