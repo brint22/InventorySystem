bae886d878bc6f84350587a6940c3d9cbb484e90
Redjyll Brint <iredjyllbrint@gmail.com>
2025-03-26 22:50:52 +0800

    Modified ViewEmployee Form

--- InventorySystem/Models/GlobalMethod.cs
+++ InventorySystem/Models/GlobalMethod.cs
@@ -21,26 +21,32 @@ namespace InventorySystem.Models
                 {
                     connection.Open();
                     string query = @"
-            SELECT e.EmployeeID, 
+                   SELECT e.EmployeeID, 
                    (e.FirstName + ' ' + 
-                    ISNULL(e.MiddleName + ' ', '') +
-                    e.LastName + ' ' + 
-                    ISNULL(e.NameExtension, '')) AS EmployeeName,
-                   e.DateOfBirth, e.Address, 
-                   UPPER(LEFT(r.RoleName, 1)) + LOWER(SUBSTRING(r.RoleName, 2, LEN(r.RoleName))) AS RoleName, -- Sentence Case
+                   ISNULL(e.MiddleName + ' ', '') +
+                   e.LastName + ' ' + 
+                   ISNULL(e.NameExtension, '')) AS EmployeeName,
+                   FORMAT(e.DateOfBirth, 'MMMM dd, yyyy') AS DateOfBirth,
+                   e.Address, 
+                   UPPER(LEFT(r.RoleName, 1)) + LOWER(SUBSTRING(r.RoleName, 2, LEN(r.RoleName))) AS RoleName,
                    ei.ImageData,
-                   DATEDIFF(YEAR, DateOfBirth, CAST(GETDATE() AS DATE)) - 
-                            CASE 
-                                 WHEN MONTH(DateOfBirth) > MONTH(CAST(GETDATE() AS DATE)) 
-                                   OR (MONTH(DateOfBirth) = MONTH(CAST(GETDATE() AS DATE)) 
-                                   AND DAY(DateOfBirth) > DAY(CAST(GETDATE() AS DATE))) 
-                                 THEN 1 ELSE 0 
-                            END
+                   CAST(
+                   DATEDIFF(YEAR, e.DateOfBirth, CAST(GETDATE() AS DATE)) - 
+                   CASE 
+                        WHEN MONTH(e.DateOfBirth) > MONTH(CAST(GETDATE() AS DATE)) 
+                          OR (MONTH(e.DateOfBirth) = MONTH(CAST(GETDATE() AS DATE)) 
+                          AND DAY(e.DateOfBirth) > DAY(CAST(GETDATE() AS DATE))) 
+                        THEN 1 ELSE 0 
+                    END 
+                    AS VARCHAR) + ' years old' AS Age,                   
+                    e.PhoneNumber,
+                   FORMAT(e.DateHired, 'MMMM dd, yyyy') AS DateHired,
+                   e.Gender,
+                   e.CivilStatus
                     FROM Employee e
                     LEFT JOIN EmployeeImage ei ON e.ImageID = ei.ImageID
                     LEFT JOIN Role r ON e.RoleID = r.RoleID
                     WHERE (@RoleFilter = 'All' OR r.RoleName = @RoleFilter)";
-
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@RoleFilter", roleFilter);
