8f8e2af9a03f7d9f134a1fdac25b2c811c9b5ab7
2022-0621 <2022-0621@LAB1-PC10>
2025-04-15 14:09:48 +0800

    i change design and grid table name

--- InventorySystem/views/Laborer.Designer.cs
+++ InventorySystem/views/Laborer.Designer.cs
@@ -30,25 +30,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Laborer));
             this.gcFacultyStaff = new DevExpress.XtraEditors.GroupControl();
-            this.gcEmployeeList = new DevExpress.XtraGrid.GridControl();
-            this.gvEmployeeList = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.txtSearch = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.btnFilter = new DevExpress.XtraEditors.SimpleButton();
             this.cbFilter = new DevExpress.XtraEditors.ComboBoxEdit();
-            this.ProductID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gcEmployeeList = new DevExpress.XtraGrid.GridControl();
+            this.gvEmployeeList = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.ProductName = new DevExpress.XtraGrid.Columns.GridColumn();
             this.Price = new DevExpress.XtraGrid.Columns.GridColumn();
             this.Quantity = new DevExpress.XtraGrid.Columns.GridColumn();
             this.ExpirationDate = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.ProductRecieved = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.CategoryID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Category = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Brand = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Supplier = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.gcFacultyStaff)).BeginInit();
             this.gcFacultyStaff.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gcEmployeeList)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gvEmployeeList)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtSearch.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cbFilter.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcEmployeeList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvEmployeeList)).BeginInit();
             this.SuspendLayout();
             // 
             // gcFacultyStaff
@@ -68,35 +68,6 @@
             this.gcFacultyStaff.Size = new System.Drawing.Size(947, 507);
             this.gcFacultyStaff.TabIndex = 6;
             this.gcFacultyStaff.Text = "   Filter By :";
-            // 
-            // gcEmployeeList
-            // 
-            this.gcEmployeeList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.gcEmployeeList.Location = new System.Drawing.Point(0, 115);
-            this.gcEmployeeList.MainView = this.gvEmployeeList;
-            this.gcEmployeeList.Name = "gcEmployeeList";
-            this.gcEmployeeList.Size = new System.Drawing.Size(942, 404);
-            this.gcEmployeeList.TabIndex = 6;
-            this.gcEmployeeList.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gvEmployeeList});
-            // 
-            // gvEmployeeList
-            // 
-            this.gvEmployeeList.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
-            this.ProductID,
-            this.ProductName,
-            this.Price,
-            this.Quantity,
-            this.ExpirationDate,
-            this.ProductRecieved,
-            this.CategoryID});
-            this.gvEmployeeList.FixedLineWidth = 1;
-            this.gvEmployeeList.GridControl = this.gcEmployeeList;
-            this.gvEmployeeList.Name = "gvEmployeeList";
-            this.gvEmployeeList.OptionsView.ShowGroupPanel = false;
-            this.gvEmployeeList.VertScrollVisibility = DevExpress.XtraGrid.Views.Base.ScrollVisibility.Never;
             // 
             // txtSearch
             // 
@@ -120,8 +91,10 @@
             // 
             // btnFilter
             // 
-            this.btnFilter.Appearance.BackColor = System.Drawing.Color.Teal;
+            this.btnFilter.Appearance.BackColor = System.Drawing.Color.Gray;
+            this.btnFilter.Appearance.ForeColor = System.Drawing.Color.Black;
             this.btnFilter.Appearance.Options.UseBackColor = true;
+            this.btnFilter.Appearance.Options.UseForeColor = true;
             this.btnFilter.Location = new System.Drawing.Point(281, 13);
             this.btnFilter.Name = "btnFilter";
             this.btnFilter.Size = new System.Drawing.Size(53, 21);
@@ -142,41 +115,54 @@
             this.cbFilter.Size = new System.Drawing.Size(162, 21);
             this.cbFilter.TabIndex = 1;
             // 
-            // ProductID
+            // gcEmployeeList
             // 
-            this.ProductID.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProductID.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
-            this.ProductID.AppearanceHeader.Options.UseFont = true;
-            this.ProductID.AppearanceHeader.Options.UseForeColor = true;
-            this.ProductID.AppearanceHeader.Options.UseTextOptions = true;
-            this.ProductID.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.ProductID.Caption = "ProductID";
-            this.ProductID.FieldName = "ProductID";
-            this.ProductID.Name = "ProductID";
-            this.ProductID.Visible = true;
-            this.ProductID.VisibleIndex = 0;
-            this.ProductID.Width = 117;
+            this.gcEmployeeList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gcEmployeeList.Location = new System.Drawing.Point(2, 108);
+            this.gcEmployeeList.MainView = this.gvEmployeeList;
+            this.gcEmployeeList.Name = "gcEmployeeList";
+            this.gcEmployeeList.Size = new System.Drawing.Size(942, 394);
+            this.gcEmployeeList.TabIndex = 7;
+            this.gcEmployeeList.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvEmployeeList});
+            // 
+            // gvEmployeeList
+            // 
+            this.gvEmployeeList.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.ProductName,
+            this.Price,
+            this.Quantity,
+            this.ExpirationDate,
+            this.Category,
+            this.Brand,
+            this.Supplier});
+            this.gvEmployeeList.FixedLineWidth = 1;
+            this.gvEmployeeList.GridControl = this.gcEmployeeList;
+            this.gvEmployeeList.Name = "gvEmployeeList";
+            this.gvEmployeeList.OptionsView.ShowGroupPanel = false;
+            this.gvEmployeeList.VertScrollVisibility = DevExpress.XtraGrid.Views.Base.ScrollVisibility.Never;
             // 
             // ProductName
             // 
             this.ProductName.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProductName.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
+            this.ProductName.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
             this.ProductName.AppearanceHeader.Options.UseFont = true;
             this.ProductName.AppearanceHeader.Options.UseForeColor = true;
             this.ProductName.AppearanceHeader.Options.UseTextOptions = true;
             this.ProductName.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.ProductName.Caption = "ProductName";
+            this.ProductName.Caption = "Product Name";
             this.ProductName.FieldName = "ProductName";
             this.ProductName.Name = "ProductName";
-            this.ProductName.OptionsColumn.AllowEdit = false;
             this.ProductName.Visible = true;
-            this.ProductName.VisibleIndex = 1;
-            this.ProductName.Width = 135;
+            this.ProductName.VisibleIndex = 0;
+            this.ProductName.Width = 120;
             // 
             // Price
             // 
             this.Price.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Price.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
+            this.Price.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
             this.Price.AppearanceHeader.Options.UseFont = true;
             this.Price.AppearanceHeader.Options.UseForeColor = true;
             this.Price.AppearanceHeader.Options.UseTextOptions = true;
@@ -186,13 +172,13 @@
             this.Price.Name = "Price";
             this.Price.OptionsColumn.AllowEdit = false;
             this.Price.Visible = true;
-            this.Price.VisibleIndex = 2;
-            this.Price.Width = 81;
+            this.Price.VisibleIndex = 1;
+            this.Price.Width = 90;
             // 
             // Quantity
             // 
             this.Quantity.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Quantity.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
+            this.Quantity.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
             this.Quantity.AppearanceHeader.Options.UseFont = true;
             this.Quantity.AppearanceHeader.Options.UseForeColor = true;
             this.Quantity.AppearanceHeader.Options.UseTextOptions = true;
@@ -202,55 +188,71 @@
             this.Quantity.Name = "Quantity";
             this.Quantity.OptionsColumn.AllowEdit = false;
             this.Quantity.Visible = true;
-            this.Quantity.VisibleIndex = 3;
-            this.Quantity.Width = 74;
+            this.Quantity.VisibleIndex = 2;
+            this.Quantity.Width = 100;
             // 
             // ExpirationDate
             // 
             this.ExpirationDate.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ExpirationDate.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
+            this.ExpirationDate.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
             this.ExpirationDate.AppearanceHeader.Options.UseFont = true;
             this.ExpirationDate.AppearanceHeader.Options.UseForeColor = true;
             this.ExpirationDate.AppearanceHeader.Options.UseTextOptions = true;
             this.ExpirationDate.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.ExpirationDate.Caption = "ExpirationDate";
+            this.ExpirationDate.Caption = "Expiration Date";
             this.ExpirationDate.FieldName = "ExpirationDate";
             this.ExpirationDate.Name = "ExpirationDate";
             this.ExpirationDate.OptionsColumn.AllowEdit = false;
             this.ExpirationDate.Visible = true;
-            this.ExpirationDate.VisibleIndex = 4;
-            this.ExpirationDate.Width = 92;
+            this.ExpirationDate.VisibleIndex = 3;
+            this.ExpirationDate.Width = 130;
             // 
-            // ProductRecieved
+            // Category
             // 
-            this.ProductRecieved.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProductRecieved.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
-            this.ProductRecieved.AppearanceHeader.Options.UseFont = true;
-            this.ProductRecieved.AppearanceHeader.Options.UseForeColor = true;
-            this.ProductRecieved.AppearanceHeader.Options.UseTextOptions = true;
-            this.ProductRecieved.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.ProductRecieved.Caption = "ProductRecieved";
-            this.ProductRecieved.FieldName = "ProductRecieved";
-            this.ProductRecieved.Name = "ProductRecieved";
-            this.ProductRecieved.OptionsColumn.AllowEdit = false;
-            this.ProductRecieved.Visible = true;
-            this.ProductRecieved.VisibleIndex = 5;
-            this.ProductRecieved.Width = 80;
+            this.Category.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Category.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
+            this.Category.AppearanceHeader.Options.UseFont = true;
+            this.Category.AppearanceHeader.Options.UseForeColor = true;
+            this.Category.AppearanceHeader.Options.UseTextOptions = true;
+            this.Category.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.Category.Caption = "Category";
+            this.Category.FieldName = "Category";
+            this.Category.Name = "Category";
+            this.Category.OptionsColumn.AllowEdit = false;
+            this.Category.Visible = true;
+            this.Category.VisibleIndex = 4;
+            this.Category.Width = 100;
             // 
-            // CategoryID
+            // Brand
             // 
-            this.CategoryID.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CategoryID.AppearanceHeader.ForeColor = System.Drawing.Color.Teal;
-            this.CategoryID.AppearanceHeader.Options.UseFont = true;
-            this.CategoryID.AppearanceHeader.Options.UseForeColor = true;
-            this.CategoryID.AppearanceHeader.Options.UseTextOptions = true;
-            this.CategoryID.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.CategoryID.Caption = "CategoryID";
-            this.CategoryID.FieldName = "CategoryID";
-            this.CategoryID.Name = "CategoryID";
-            this.CategoryID.Visible = true;
-            this.CategoryID.VisibleIndex = 6;
-            this.CategoryID.Width = 111;
+            this.Brand.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Brand.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
+            this.Brand.AppearanceHeader.Options.UseFont = true;
+            this.Brand.AppearanceHeader.Options.UseForeColor = true;
+            this.Brand.AppearanceHeader.Options.UseTextOptions = true;
+            this.Brand.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.Brand.Caption = "Brand";
+            this.Brand.FieldName = "Brand";
+            this.Brand.Name = "Brand";
+            this.Brand.OptionsColumn.AllowEdit = false;
+            this.Brand.Visible = true;
+            this.Brand.VisibleIndex = 5;
+            this.Brand.Width = 100;
+            // 
+            // Supplier
+            // 
+            this.Supplier.AppearanceHeader.Font = new System.Drawing.Font("Sitka Text", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Supplier.AppearanceHeader.ForeColor = System.Drawing.Color.Black;
+            this.Supplier.AppearanceHeader.Options.UseFont = true;
+            this.Supplier.AppearanceHeader.Options.UseForeColor = true;
+            this.Supplier.AppearanceHeader.Options.UseTextOptions = true;
+            this.Supplier.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.Supplier.Caption = "Supplier";
+            this.Supplier.FieldName = "Supplier";
+            this.Supplier.Name = "Supplier";
+            this.Supplier.Visible = true;
+            this.Supplier.VisibleIndex = 6;
+            this.Supplier.Width = 100;
             // 
             // Laborer
             // 
@@ -264,10 +266,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.gcFacultyStaff)).EndInit();
             this.gcFacultyStaff.ResumeLayout(false);
             this.gcFacultyStaff.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gcEmployeeList)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gvEmployeeList)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtSearch.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cbFilter.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcEmployeeList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvEmployeeList)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -275,18 +277,18 @@
         #endregion
 
         private DevExpress.XtraEditors.GroupControl gcFacultyStaff;
-        private DevExpress.XtraGrid.GridControl gcEmployeeList;
-        private DevExpress.XtraGrid.Views.Grid.GridView gvEmployeeList;
-        private DevExpress.XtraGrid.Columns.GridColumn ProductID;
-        private DevExpress.XtraGrid.Columns.GridColumn ProductName;
-        private DevExpress.XtraGrid.Columns.GridColumn Price;
-        private DevExpress.XtraGrid.Columns.GridColumn Quantity;
-        private DevExpress.XtraGrid.Columns.GridColumn ExpirationDate;
-        private DevExpress.XtraGrid.Columns.GridColumn ProductRecieved;
-        private DevExpress.XtraGrid.Columns.GridColumn CategoryID;
         private DevExpress.XtraEditors.TextEdit txtSearch;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.SimpleButton btnFilter;
         private DevExpress.XtraEditors.ComboBoxEdit cbFilter;
+        private DevExpress.XtraGrid.GridControl gcEmployeeList;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvEmployeeList;
+        private DevExpress.XtraGrid.Columns.GridColumn ProductName;
+        private DevExpress.XtraGrid.Columns.GridColumn Price;
+        private DevExpress.XtraGrid.Columns.GridColumn Quantity;
+        private DevExpress.XtraGrid.Columns.GridColumn ExpirationDate;
+        private DevExpress.XtraGrid.Columns.GridColumn Category;
+        private DevExpress.XtraGrid.Columns.GridColumn Brand;
+        private DevExpress.XtraGrid.Columns.GridColumn Supplier;
     }
 }
