5ccdd2a1b980657ea2104b89bd4253afb19b9e37
ADMIN <ADMIN@NIEZEL>
2025-04-27 21:45:41 +0800

    Change the query in view location to ascending

--- InventorySystem/Infrastracture/SQL/ProductSQL.cs
+++ InventorySystem/Infrastracture/SQL/ProductSQL.cs
@@ -17,8 +17,13 @@ namespace InventorySystem.Infrastracture.SQL
         WHERE LocationID = @LocationID";
 
         public static string GetAllLocations = @"
-        SELECT LocationID, ProductID, Availability 
-        FROM Location";
+       SELECT [LocationID],
+              ProductID,
+              Availability
+              FROM [WAREHOUSEISDB].[dbo].[Location]
+              ORDER BY 
+              CAST(SUBSTRING([LocationID], CHARINDEX('-', [LocationID], 1) + 1, CHARINDEX('-', [LocationID], CHARINDEX('-', [LocationID]) + 1) - CHARINDEX('-', [LocationID]) - 1) AS INT) ASC,
+              CAST(SUBSTRING([LocationID], CHARINDEX('-', [LocationID], CHARINDEX('-', [LocationID]) + 1) + 1, LEN([LocationID])) AS INT) ASC";
 
         public static string InsertLocation = @"
         INSERT INTO Location (LocationID, ProductID, Availability) 
