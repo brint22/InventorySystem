f1a32045e7224088496667656f4c0217d8b41da6
Redjyll Brint <iredjyllbrint@gmail.com>
2025-05-03 21:59:03 +0800

    view stock modified phase 1

--- InventorySystem/Products/Stock/ViewStock.Designer.cs
+++ InventorySystem/Products/Stock/ViewStock.Designer.cs
@@ -31,7 +31,18 @@
             this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.gcCategory = new DevExpress.XtraGrid.GridControl();
+            this.gvCategory = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.StockID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.ProductName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Price = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Quantity = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.ExpirationDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Location = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Supplier = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcCategory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCategory)).BeginInit();
             this.SuspendLayout();
             // 
             // ribbon
@@ -44,7 +55,7 @@
             this.ribbon.Name = "ribbon";
             this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbon.Size = new System.Drawing.Size(1069, 242);
+            this.ribbon.Size = new System.Drawing.Size(1069, 61);
             // 
             // ribbonPage1
             // 
@@ -58,16 +69,149 @@
             this.ribbonPageGroup1.Name = "ribbonPageGroup1";
             this.ribbonPageGroup1.Text = "ribbonPageGroup1";
             // 
+            // gcCategory
+            // 
+            this.gcCategory.Location = new System.Drawing.Point(33, 159);
+            this.gcCategory.MainView = this.gvCategory;
+            this.gcCategory.MenuManager = this.ribbon;
+            this.gcCategory.Name = "gcCategory";
+            this.gcCategory.Size = new System.Drawing.Size(1007, 288);
+            this.gcCategory.TabIndex = 3;
+            this.gcCategory.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvCategory});
+            // 
+            // gvCategory
+            // 
+            this.gvCategory.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.StockID,
+            this.ProductName,
+            this.Price,
+            this.ExpirationDate,
+            this.Quantity,
+            this.Location,
+            this.Supplier});
+            this.gvCategory.GridControl = this.gcCategory;
+            this.gvCategory.Name = "gvCategory";
+            this.gvCategory.OptionsView.ShowGroupPanel = false;
+            // 
+            // StockID
+            // 
+            this.StockID.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.StockID.AppearanceCell.Options.UseFont = true;
+            this.StockID.AppearanceCell.Options.UseTextOptions = true;
+            this.StockID.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
+            this.StockID.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.StockID.AppearanceHeader.Options.UseFont = true;
+            this.StockID.Caption = "Stock ID";
+            this.StockID.FieldName = "StockID";
+            this.StockID.MinWidth = 25;
+            this.StockID.Name = "StockID";
+            this.StockID.OptionsColumn.AllowEdit = false;
+            this.StockID.Visible = true;
+            this.StockID.VisibleIndex = 0;
+            this.StockID.Width = 290;
+            // 
+            // ProductName
+            // 
+            this.ProductName.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.ProductName.AppearanceCell.Options.UseFont = true;
+            this.ProductName.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.ProductName.AppearanceHeader.Options.UseFont = true;
+            this.ProductName.Caption = "Product Name";
+            this.ProductName.FieldName = "ProductName";
+            this.ProductName.MinWidth = 25;
+            this.ProductName.Name = "ProductName";
+            this.ProductName.OptionsColumn.AllowEdit = false;
+            this.ProductName.Visible = true;
+            this.ProductName.VisibleIndex = 1;
+            this.ProductName.Width = 165;
+            // 
+            // Price
+            // 
+            this.Price.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Price.AppearanceCell.Options.UseFont = true;
+            this.Price.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Price.AppearanceHeader.Options.UseFont = true;
+            this.Price.Caption = "Price";
+            this.Price.FieldName = "Price";
+            this.Price.MinWidth = 25;
+            this.Price.Name = "Price";
+            this.Price.Visible = true;
+            this.Price.VisibleIndex = 2;
+            this.Price.Width = 130;
+            // 
+            // Quantity
+            // 
+            this.Quantity.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Quantity.AppearanceCell.Options.UseFont = true;
+            this.Quantity.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Quantity.AppearanceHeader.Options.UseFont = true;
+            this.Quantity.Caption = "Quantity";
+            this.Quantity.FieldName = "Quantity";
+            this.Quantity.MinWidth = 25;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Visible = true;
+            this.Quantity.VisibleIndex = 3;
+            this.Quantity.Width = 130;
+            // 
+            // ExpirationDate
+            // 
+            this.ExpirationDate.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.ExpirationDate.AppearanceCell.Options.UseFont = true;
+            this.ExpirationDate.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.ExpirationDate.AppearanceHeader.Options.UseFont = true;
+            this.ExpirationDate.Caption = "Expiration Date";
+            this.ExpirationDate.FieldName = "ExpirationDate";
+            this.ExpirationDate.MinWidth = 25;
+            this.ExpirationDate.Name = "ExpirationDate";
+            this.ExpirationDate.Visible = true;
+            this.ExpirationDate.VisibleIndex = 4;
+            this.ExpirationDate.Width = 130;
+            // 
+            // Location
+            // 
+            this.Location.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Location.AppearanceCell.Options.UseFont = true;
+            this.Location.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Location.AppearanceHeader.Options.UseFont = true;
+            this.Location.Caption = "Location";
+            this.Location.FieldName = "Location";
+            this.Location.MinWidth = 25;
+            this.Location.Name = "Location";
+            this.Location.Visible = true;
+            this.Location.VisibleIndex = 5;
+            this.Location.Width = 130;
+            // 
+            // Supplier
+            // 
+            this.Supplier.AppearanceCell.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Supplier.AppearanceCell.Options.UseFont = true;
+            this.Supplier.AppearanceHeader.Font = new System.Drawing.Font("Arial", 10.2F);
+            this.Supplier.AppearanceHeader.Options.UseFont = true;
+            this.Supplier.Caption = "Supplier";
+            this.Supplier.FieldName = "Supplier";
+            this.Supplier.MinWidth = 25;
+            this.Supplier.Name = "Supplier";
+            this.Supplier.Visible = true;
+            this.Supplier.VisibleIndex = 6;
+            this.Supplier.Width = 145;
+            // 
             // ViewStock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1069, 584);
+            this.Controls.Add(this.gcCategory);
             this.Controls.Add(this.ribbon);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "ViewStock";
             this.Ribbon = this.ribbon;
-            this.Text = "ViewStock";
+            this.RibbonVisibility = DevExpress.XtraBars.Ribbon.RibbonVisibility.Hidden;
+            this.Text = "View Stock";
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcCategory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCategory)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,5 +222,14 @@
         private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
         private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraGrid.GridControl gcCategory;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvCategory;
+        private DevExpress.XtraGrid.Columns.GridColumn StockID;
+        private DevExpress.XtraGrid.Columns.GridColumn ProductName;
+        private DevExpress.XtraGrid.Columns.GridColumn Price;
+        private DevExpress.XtraGrid.Columns.GridColumn ExpirationDate;
+        private DevExpress.XtraGrid.Columns.GridColumn Quantity;
+        private DevExpress.XtraGrid.Columns.GridColumn Location;
+        private DevExpress.XtraGrid.Columns.GridColumn Supplier;
     }
 }
