d06c8a617a0938ad96defa33d961bb63a8932342
ADMIN <ADMIN@NIEZEL>
2025-05-19 17:02:25 +0800

    Add Order and Sale Classes

---
+++ InventorySystem/Models/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Models
+{
+    public class Sale
+    {
+        public int SalesID { get; set; }
+        public decimal QuantitySold { get; set; }
+        public int ProductID { get; set; }
+        public int OrderID { get; set; }
+
+
+    }
+}
